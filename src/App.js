fe719f4cd1cf6898a1360bdd5f4b0b32524b0a32
Rumana K <rumana2k20@gmail.com>
2022-03-20 03:50:09 +0600

    Using useEffect func

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
@@ -19,7 +19,7 @@ function App() {
 
     <div className="App">
 
-    {/* //   {
+      {/* //   {
       employees.map(employee => <Person name={employee}></Person>)
     }
 
@@ -28,16 +28,18 @@ function App() {
      <Person name="Aibee E." Job= "VA"></Person>
      <Person name="Irem" Job= "VA"></Person> */}
 
-     {/* {products.map(product =><Product name={product.name} price={product.price} ></Product>)} */}
+      {/* {products.map(product =><Product name={product.name} price={product.price} ></Product>)} */}
 
 
-     {/* <Product name="Laptop" price="50000" ></Product>
+      {/* <Product name="Laptop" price="50000" ></Product>
      <Product name="Phone" price="30000" ></Product>
      <Product name="Watch" price="3000" ></Product>
      <Product name="Tab" price="40000" ></Product> */}
-     
-     
-     <Counter></Counter>
+
+
+      {/* <Counter></Counter> */}
+
+      <Externalusers></Externalusers>
 
 
 
@@ -48,35 +50,75 @@ function App() {
 
 }
 
-function Counter() {
-  const [count, setCount] = useState(0)
-  const increaseCount = () => {setCount(count+1);}
-  const decreaseCount = () => {setCount(count-1);}
-    
-     
+// =============================
+// -----------Counter----------
+// =============================
+
+// function Counter() {
+//   const [count, setCount] = useState(0)
+//   const increaseCount = () => {setCount(count+1);}
+//   const decreaseCount = () => {setCount(count-1);}
 
 
-  // const increaseCount = () => {
-  //  const newCount = count+1;
-  //   setCount(newCount);
-  
 
+
+// const increaseCount = () => {
+//  const newCount = count+1;
+//   setCount(newCount);
+
+function Externalusers() {
+  const [users, setUsers] = useState([]);
+  // useEffect(() => {}, []);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, []);
+  return (
+    <div>
+      <h2>External Users</h2>
+      <p>Total Users: {users.length}</p>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+
+  )
+}
+
+function User(props) {
 
   return (
-    // <div className="person">
-    //   {/* <h1>Name: {props.name} </h1>
-    //   <h2>Jobs: {props.Job}</h2> */}
+    <div style={{border: '2px solid green', margin: '10px'}}>
+      <h2>Name: {props.name} </h2>
+      <h3>Email: {props.email}</h3>
 
-    //   {/* <h3>Name: {props.name} </h3>
-    //   <h3>Price: {props.price} </h3> */}
-    // </div>
-
-    <div>
-      <h1>Count: {count} </h1>
-      <button onClick={increaseCount} >Increase</button>
-      <button onClick={decreaseCount} >Decrease</button>
     </div>
-  );
+
+
+  )
+
 }
+
+
+
+//   return (
+//     <div className="person">
+//       {/* <h1>Name: {props.name} </h1>
+//       <h2>Jobs: {props.Job}</h2> */}
+
+//       {/* <h3>Name: {props.name} </h3>
+//       <h3>Price: {props.price} </h3> */}
+//     </div>
+// =============================
+// -----------Counter----------
+// =============================
+//     <div>
+//       <h1>Count: {count} </h1>
+//       <button onClick={increaseCount} >Increase</button>
+//       <button onClick={decreaseCount} >Decrease</button>
+//     </div>
+//   );
+
 
 export default App;
