4b4947d25c5dddf9561405981a16e987c3999ab7
Rumana K <rumana2k20@gmail.com>
2022-03-20 02:34:56 +0600

    Dynamically data used

--- src/App.js
+++ src/App.js
@@ -5,32 +5,57 @@ function App() {
 
   // const employees = ['Haoming', 'Rumana', 'Aibee', 'Irem', 'Edisa'];
 
-  // return (
+  const products = [
+    {name: 'Laptop', price: 50000},
+    {name: 'Phone', price: 30000},
+    {name: 'Tab', price: 40000},
+    {name: 'Watch', price: 3000},
 
-  //   <div className="App">
 
-  //   {
-  //     employees.map(employee => <Person name={employee}></Person>)
-  //   }
+  ]
 
-  //     <Person name="Haoming Wang" Job= "Advisor"></Person>
-  //   <Person name="Rumana K." Job= "VA"></Person>
-  //   <Person name="Aibee E." Job= "VA"></Person>
-  //   <Person name="Irem" Job= "VA"></Person>
+  return (
 
-  //   </div>
+    <div className="App">
 
-  // );
+  {/* //   {
+      employees.map(employee => <Person name={employee}></Person>)
+    }
+
+     <Person name="Haoming Wang" Job= "Advisor"></Person>
+     <Person name="Rumana K." Job= "VA"></Person>
+     <Person name="Aibee E." Job= "VA"></Person>
+     <Person name="Irem" Job= "VA"></Person> */}
+
+     {products.map(product =><Product name={product.name} price={product.price} ></Product>)}
+
+
+     {/* <Product name="Laptop" price="50000" ></Product>
+     <Product name="Phone" price="30000" ></Product>
+     <Product name="Watch" price="3000" ></Product>
+     <Product name="Tab" price="40000" ></Product> */}
+     
+     
+
+
+
+
+    </div>
+
+  );
 
 
 }
 
-function Person(props) {
+function Product(props) {
 
   return (
     <div className="person">
-      <h1>Name: {props.name} </h1>
-      <h2>Jobs: {props.Job}</h2>
+      {/* <h1>Name: {props.name} </h1>
+      <h2>Jobs: {props.Job}</h2> */}
+
+      <h3>Name: {props.name} </h3>
+      <h3>Price: {props.price} </h3>
     </div>
   );
 }
