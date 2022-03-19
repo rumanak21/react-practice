5d7a68eb9e01c873d5a624be1b43dc1ca0d15e55
Rumana K <rumana2k20@gmail.com>
2022-03-20 01:51:22 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,37 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const employees = ['Haoming', 'Rumana', 'Aibee', 'Irem', 'Edisa'];
+
   return (
+
+
+
+
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    {
+      employees.map(employee => <Person name={employee}></Person>)
+    }
+
+      {/* <Person name="Haoming Wang" Job= "Advisor"></Person>
+    <Person name="Rumana K." Job= "VA"></Person>
+    <Person name="Aibee E." Job= "VA"></Person>
+    <Person name="Irem" Job= "VA"></Person> */}
+
+    </div>
+
+  );
+}
+
+function Person(props) {
+
+  return (
+    <div className="person">
+      <h1>Name: {props.name} </h1>
+      <h2>Jobs: {props.Job}</h2>
     </div>
   );
 }
