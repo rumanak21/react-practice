91cd87eb9394aaa2491a5c81db15099d33c145f2
rumanak21 <rumana2k20@gmail.com>
2022-05-15 22:15:34 +0600

    Lorem added

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            
+            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa dolorum earum quos iure veritatis temporibus eius ipsum, suscipit deleniti vero quod nisi delectus reprehenderit molestias voluptatem quisquam nihil porro exercitationem facilis velit, voluptas alias labore ad necessitatibus. Ad voluptas ut, cumque quae praesentium ab qui? Nemo cupiditate aut et ab?</h2>
         </div>
     );
 };
